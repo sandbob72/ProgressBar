1f4a84dd13a3f1f15289ce9471e0004fd8904d4f
SANDBOB <sakul.s2557@gmail.com>
2019-06-27 16:53:53 +0700

    setOnClick

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -14,8 +14,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var progressStatus = 0
-    var handler: Handler? = null
+    var isStartedBnext = false
+    var isStartedBpre = false
+    var progressStatus2 = 0
+    var handLer: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +27,93 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener {
             startActivity(Intent(this@MainActivity, Main2Activity::class.java))
         }
+        //--------------------------------------------------------------------------------------//
+        //--------------------------------------------------------------------------------------//
+        var progressStatus = 0
+
+        var progressDefault = 25
+
+        val handler: Handler = Handler()
+
+        bNext.setOnClickListener {
+
+            Thread(Runnable {
+                while (progressStatus < progressDefault) {
+                    progressStatus += 1
+
+                    try {
+                        Thread.sleep(50)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                    handler.post(Runnable {
+                        progressBarHorizontal.progress = progressStatus
+                    })
+                }
+            }).start()
+
+            if (progressStatus == progressDefault){
+                progressDefault += 25
+                return@setOnClickListener
+            }
+
+        }
+
+        bPre.setOnClickListener {
+
+            Thread(Runnable {
+                while (progressStatus > progressDefault) {
+                    progressStatus -= 1
+
+                    try {
+                        Thread.sleep(50)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                    handler.post(Runnable {
+                        progressBarHorizontal.progress = progressStatus
+                    })
+                }
+            }).start()
+
+            if (progressStatus == progressDefault) {
+                progressDefault -= 25
+                return@setOnClickListener
+            }
+        }
+
+        //--------------------------------------------------------------------------------------//
+        //--------------------------------------------------------------------------------------//
+
+        handLer = Handler(Handler.Callback {
+            if (isStartedBnext) {
+                progressStatus2++
+            }
+            if (progressStatus2 == 25) {
+                isStartedBnext = false
+            }
+            if (progressStatus2 == 50) {
+                isStartedBnext = false
+            }
+            if (progressStatus2 == 75) {
+                isStartedBnext = false
+            }
+            if (progressStatus2 == 100) {
+                isStartedBnext = false
+            }
+            progressBarHorizontal2.progress = progressStatus2
+            handLer?.sendEmptyMessageDelayed(0, 80)
+
+            true
+        })
+        handLer?.sendEmptyMessage(0)
+
+    }
 
 
-        progressBarHorizontal.progress = 0
-
+    fun bNEXT(view: View) {
+        isStartedBnext = !isStartedBnext
     }
 }
